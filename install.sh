9179b336fdab0828dda3a3c22f8d49afee32d7c2
Flavius Aspra <flavius.as@gmail.com>
2012-08-15 18:14:04 +0200

    fixed

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-mkdir -p .hacklets/hacklets.git
-git clone --bare git://github.com/hacklets/hacklets-test.git .hacklets/hacklets.git
-ln -s hacklets.git .hacklets/master
-git --git-dir=.hacklets/master config core.bare false
-git --git-dir=.hacklets/master config core.worktree "../../"
-git --git-dir=.hacklets/master checkout
+#mkdir -p .hacklets/hacklets.git
+#git clone --bare git://github.com/hacklets/hacklets-test.git .hacklets/hacklets.git
+#ln -s hacklets.git .hacklets/master
+#git --git-dir=.hacklets/master config core.bare false
+#git --git-dir=.hacklets/master config core.worktree "../../"
+#git --git-dir=.hacklets/master checkout
 
 echo "Your real name will be used as a display name in your commits, on github, \
 and in any similar situations."
-SUGGEST=`getent passwd foo | cut -d ':' -f 5`
+SUGGEST=`getent passwd "$USER" | cut -d ':' -f 5`
 read -e -p "Your real name: " -i "${SUGGEST}" REAL_NAME
 echo "Your e-mail should be the same as the one used to register on github, \
 for easier github integration."
-read -e -p "Your E-Mail:" -i "${USER}@${HOSTNAME}" EMAIL
+read -e -p "Your E-Mail: " -i "${USER}@${HOSTNAME}" EMAIL
 
-echo "git config --global user.email "$EMAIL""
-git config --global user.email "$EMAIL"
-echo "git config --global user.name "$REAL_NAME""
-git config --global user.name "$REAL_NAME"
+echo git config --global user.email "$EMAIL"
+#git config --global user.email "$EMAIL"
+echo git config --global user.name "$REAL_NAME"
+#git config --global user.name "$REAL_NAME"
