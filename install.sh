db219ee9c10da4dbd05a0089c6e7626295d17150
Fl <flav@evolution>
2012-08-15 18:44:12 +0200

    still buggy

--- install.sh
+++ install.sh
@@ -9,12 +9,10 @@ git --git-dir=.hacklets/master config core.bare false
 git --git-dir=.hacklets/master config core.worktree "../../"
 git --git-dir=.hacklets/master checkout
 
-echo "Your real name will be used as a display name in your commits, on github, \
-and in any similar situations."
+echo "Your real name will be used as a display name in your commits, on github, and in any similar situations."
 SUGGEST=`getent passwd "$USER" | cut -d ':' -f 5`
 read -e -p "Your real name: " -i "${SUGGEST}" REAL_NAME
-echo "Your e-mail should be the same as the one used to register on github, \
-for easier github integration."
+echo "Your e-mail should be the same as the one used to register on github, for easier github integration."
 read -e -p "Your E-Mail: " -i "${USER}@${HOSTNAME}" EMAIL
 
 source bin/scripts.d/bsfl
