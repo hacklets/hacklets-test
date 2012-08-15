4e7379a91b6b110871e0d136f20447678eb71044
Fl <flav@evolution>
2012-08-15 18:35:18 +0200

    fixed install

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-#mkdir -p .hacklets/hacklets.git
-#git clone --bare git://github.com/hacklets/hacklets-test.git .hacklets/hacklets.git
-#ln -s hacklets.git .hacklets/master
-#git --git-dir=.hacklets/master config core.bare false
-#git --git-dir=.hacklets/master config core.worktree "../../"
-#git --git-dir=.hacklets/master checkout
+#TODO: error checking throughout the file
+
+mkdir -p .hacklets/hacklets.git
+git clone --bare git://github.com/hacklets/hacklets-test.git .hacklets/hacklets.git
+ln -s hacklets.git .hacklets/master
+git --git-dir=.hacklets/master config core.bare false
+git --git-dir=.hacklets/master config core.worktree "../../"
+git --git-dir=.hacklets/master checkout
 
 echo "Your real name will be used as a display name in your commits, on github, \
 and in any similar situations."
@@ -15,7 +17,7 @@ echo "Your e-mail should be the same as the one used to register on github, \
 for easier github integration."
 read -e -p "Your E-Mail: " -i "${USER}@${HOSTNAME}" EMAIL
 
-echo git config --global user.email "$EMAIL"
-#git config --global user.email "$EMAIL"
-echo git config --global user.name "$REAL_NAME"
-#git config --global user.name "$REAL_NAME"
+source bin/scripts.d/bsfl
+
+cmd "git config --global user.email ${EMAIL}"
+cmd "git config --global user.name ${REAL_NAME}"
