23a68d5993b9e47d225a24007b6753f236bcd134
Flavius Aspra <flavius.as@gmail.com>
2012-08-31 20:12:52 +0200

    using system's git

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 #TODO: error checking throughout the file
 
 [ ! -d .hacklets/hacklets.git ] && mkdir -p .hacklets/hacklets.git
-[ ! -f .hacklets/hacklets.git/config ] && git clone -b master --bare git://github.com/hacklets/hacklets-test.git .hacklets/hacklets.git
+[ ! -f .hacklets/hacklets.git/config ] && /usr/bin/git clone -b master --bare git://github.com/hacklets/hacklets-test.git .hacklets/hacklets.git
 [ ! -h .hacklets/master ] && ln -s hacklets.git .hacklets/master
 
-git --git-dir=.hacklets/master config core.bare false
-git --git-dir=.hacklets/master config core.worktree "../../"
-git --git-dir=.hacklets/master checkout
+/usr/bin/git --git-dir=.hacklets/master config core.bare false
+/usr/bin/git --git-dir=.hacklets/master config core.worktree "../../"
+/usr/bin/git --git-dir=.hacklets/master checkout
 
 echo "Your real name will be used as a display name in your commits, on github, and in any similar situations."
 SUGGEST=`getent passwd "$USER" | cut -d ':' -f 5`
@@ -18,7 +18,7 @@ read -e -p "Your E-Mail: " -i "${USER}@${HOSTNAME}" EMAIL
 
 source bin/scripts.d/bsfl
 
-git config --replace-all --global user.email "${EMAIL}"
+/usr/bin/git config --replace-all --global user.email "${EMAIL}"
 if [[ 0 != $? ]]; then 
     msg_fail "git config --replace-all --global user.email '${EMAIL}'"
     return $?;
@@ -26,7 +26,7 @@ else
     msg_ok "git config --replace-all --global user.email '${EMAIL}'"
 fi
 
-git config --replace-all --global user.name "${REAL_NAME}"
+/usr/bin/git config --replace-all --global user.name "${REAL_NAME}"
 if [[ 0 != $? ]]; then 
     msg_fail "git config --replace-all --global user.name '${REAL_NAME}'"
     return $?;
